e9b18612f0b1f8756ab35e5ec75a581a85c25fcd
tcdat96 <tcdat96@gmail.com>
2021-03-09 20:48:39 -0600

    allow character to move + remove original camera

--- Assets/InstancePlacement.cs
+++ Assets/InstancePlacement.cs
@@ -12,8 +12,11 @@ public class InstancePlacement : MonoBehaviour
 
     private Pose placementPose;
     private bool isPlacementPoseValid = false;
-
     public GameObject placementIndicator;
+
+    private Pose destinationPose;
+    private bool isMoving = false;
+    public float speed = 10;
     public GameObject character;
 
     void Start()
@@ -23,16 +26,48 @@ public class InstancePlacement : MonoBehaviour
 
     void Update()
     {
-        UpdatePlacementPose();
-        UpdatePlacementIndicator();
-        PlaceCharacter();
+        if (!character.activeInHierarchy)
+        {
+            GetPlacementPose();
+            UpdatePlacementIndicator();
+        }
+
+        Touch touch;
+        if (Input.touchCount > 0 && (touch = Input.GetTouch(0)).phase == TouchPhase.Began)
+        {
+            // if we haven't placed the character
+            if (!character.activeInHierarchy)
+            {
+                // check if it can be placed
+                if (isPlacementPoseValid)
+                {
+                    showCharacter();
+                }
+            }
+            // otherwise, we need to prepare to move it
+            else
+            {
+                GetDestinationPose(touch.position);
+            }
+        }
+
+        if (isMoving)
+        {
+            MoveCharacter(destinationPose.position);
+        }
     }
 
-    private void UpdatePlacementPose()
+    private List<ARRaycastHit> RayCast(Vector2 screenPoint)
+    {
+        var hits = new List<ARRaycastHit>();
+        raycastManager.Raycast(screenPoint, hits, TrackableType.Planes);
+        return hits;
+    }
+
+    private void GetPlacementPose()
     {
         var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(.5f, .5f));
-        var hits = new List<ARRaycastHit>();
-        raycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+        var hits = RayCast(screenCenter);
 
         isPlacementPoseValid = hits.Count > 0;
 
@@ -64,20 +99,40 @@ public class InstancePlacement : MonoBehaviour
         }
     }
 
-    private void PlaceCharacter()
+    private void showCharacter()
     {
-        Touch touch;
-        if (!isPlacementPoseValid || Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        if (character == null || !isPlacementPoseValid)
         {
             return;
         }
 
-        //if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
-        //{
-        //    return;
-        //}
+        placementIndicator.SetActive(false);
+        
+        character.SetActive(true);
+        character.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        character.transform.Rotate(0, 180, 0);
+    }
 
-        Instantiate(character, placementPose.position, placementPose.rotation);
-        Instantiate(placementIndicator, placementPose.position, placementPose.rotation);
+    private void GetDestinationPose(Vector3 touchPosition)
+    {
+        var hits = RayCast(touchPosition);
+        if (hits.Count > 0)
+        {
+            destinationPose = hits[0].pose;
+            isMoving = true;
+        }
+    }
+
+    private void MoveCharacter(Vector3 target)
+    {
+        // move it to desired position
+        Transform transform = character.transform;
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        
+        // check if we've reached the destionation
+        if (transform.position == target)
+        {
+            isMoving = false;
+        }
     }
 }
